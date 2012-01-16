5bb47f8c75f5cfff2023cd32743d97aabd1f5733
Leif Baierl <hoshmeister@gmail.com>
2012-01-16 20:42:14 +0100

    added defecation type spinner

--- src/com/gittekat/tummyjournal/activities/DefecationActivity.java
+++ src/com/gittekat/tummyjournal/activities/DefecationActivity.java
@@ -1,10 +1,11 @@
 package com.gittekat.tummyjournal.activities;
 
-import com.gittekat.tummyjournal.R;
-import com.gittekat.tummyjournal.R.layout;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.gittekat.tummyjournal.R;
 
 public class DefecationActivity extends Activity {
 	@Override
@@ -12,5 +13,10 @@ public class DefecationActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.defecation);
 
+		// defecation types spinner
+		final Spinner defecationTypeSpinner = (Spinner) findViewById(R.id.spinner1);
+		final ArrayAdapter types_adapter = ArrayAdapter.createFromResource(this, R.array.defecation_types, android.R.layout.simple_spinner_item);
+		types_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		defecationTypeSpinner.setAdapter(types_adapter);
 	}
 }
