e0782868adafa1169ee5820a30f02b67d179030b
Leif Baierl <hoshmeister@gmail.com>
2012-01-21 01:02:29 +0100

    time picker dialog

--- src/com/gittekat/tummyjournal/activities/DefecationActivity.java
+++ src/com/gittekat/tummyjournal/activities/DefecationActivity.java
@@ -5,24 +5,28 @@ import java.util.Calendar;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.gittekat.tummyjournal.R;
 
 public class DefecationActivity extends Activity {
 	private TextView dateDisplay;
-	private Button pickDate;
+	private TextView timeDisplay;
+	private int hour;
+	private int minute;
 	private int year;
 	private int month;
 	private int day;
 
 	static final int DATE_DIALOG_ID = 0;
+	static final int TIME_DIALOG_ID = 1;
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
@@ -31,11 +35,16 @@ public class DefecationActivity extends Activity {
 
 		// date picker
 		dateDisplay = (TextView) findViewById(R.id.dateDisplay);
-		pickDate = (Button) findViewById(R.id.pickDate);
-
-		pickDate.setOnClickListener(new View.OnClickListener() {
+		dateDisplay.setOnClickListener(new View.OnClickListener() {
 			public void onClick(final View v) {
 				showDialog(DATE_DIALOG_ID);
+			}
+		});
+
+		timeDisplay = (TextView) findViewById(R.id.timeDisplay);
+		timeDisplay.setOnClickListener(new View.OnClickListener() {
+			public void onClick(final View v) {
+				showDialog(TIME_DIALOG_ID);
 			}
 		});
 
@@ -43,12 +52,14 @@ public class DefecationActivity extends Activity {
 		year = c.get(Calendar.YEAR);
 		month = c.get(Calendar.MONTH);
 		day = c.get(Calendar.DAY_OF_MONTH);
+		hour = c.get(Calendar.HOUR_OF_DAY);
+		minute = c.get(Calendar.MINUTE);
 
 		updateDisplay();
 
 		// defecation types spinner
 		final Spinner defecationTypeSpinner = (Spinner) findViewById(R.id.spinner1);
-		final ArrayAdapter types_adapter = ArrayAdapter.createFromResource(this, R.array.defecation_types, android.R.layout.simple_spinner_item);
+		final ArrayAdapter types_adapter = ArrayAdapter.createFromResource(this, R.array.defecation_types, R.layout.spinner_layout);
 		types_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		defecationTypeSpinner.setAdapter(types_adapter);
 	}
@@ -57,23 +68,39 @@ public class DefecationActivity extends Activity {
 	protected Dialog onCreateDialog(final int id) {
 		switch (id) {
 		case DATE_DIALOG_ID:
-			return new DatePickerDialog(this, mDateSetListener, year, month, day);
+			return new DatePickerDialog(this, dateSetListener, year, month, day);
+		case TIME_DIALOG_ID:
+			return new TimePickerDialog(this, timeSetListener, hour, minute, true);
 		}
 		return null;
 	}
 
 	private void updateDisplay() {
-		dateDisplay.setText(new StringBuilder()
-		// Month is 0 based so add 1
-				.append(day).append("-").append(month + 1).append("-").append(year).append(" "));
+		dateDisplay.setText(new StringBuilder().append(getString(R.string.defecation_date)).append(": ").append(day).append(".").append(month + 1)
+				.append(".").append(year).append(" "));
+		String minuteText = "";
+		if (minute < 10) {
+			minuteText = "0" + minute;
+		} else {
+			minuteText = "" + minute;
+		}
+		timeDisplay.setText(new StringBuilder().append(getString(R.string.defecation_time)).append(": ").append(hour).append(":").append(minuteText));
 	}
 
-	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+	private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
 
 		public void onDateSet(final DatePicker view, final int y, final int monthOfYear, final int dayOfMonth) {
 			year = y;
 			month = monthOfYear;
 			day = dayOfMonth;
+			updateDisplay();
+		}
+	};
+
+	private TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+		public void onTimeSet(final TimePicker view, final int h, final int min) {
+			hour = h;
+			minute = min;
 			updateDisplay();
 		}
 	};
